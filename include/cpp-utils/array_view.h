450d6b94e90d31bfd2692d692837e02e2e3690ec
R1tschY <r1tschy@yahoo.de>
2015-09-23 22:59:58 +0200

    added array_view from GSL

---
+++ include/cpp-utils/array_view.h
@@ -0,0 +1,12 @@
+#ifndef LIBS_CPP_UTILS_ARRAY_VIEW_H_
+#define LIBS_CPP_UTILS_ARRAY_VIEW_H_
+
+#include "../../3th-party/gsl/array_view.h"
+
+namespace cpp {
+
+using Guide::array_view;
+
+} // namespace cpp
+
+#endif /* LIBS_CPP_UTILS_ARRAY_VIEW_H_ */
