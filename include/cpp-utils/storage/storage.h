650be09ab93e2fdc8ca1a784cfe96f8142acdf2a
R1tschY <r1tschy@yahoo.de>
2016-06-22 12:27:11 +0200

    storage module for operations on uninitailized memory

---
+++ include/cpp-utils/storage/storage.h
@@ -0,0 +1,23 @@
+#ifndef CPP_STORAGE_H
+#define CPP_STORAGE_H
+
+#include <type_traits>
+
+namespace cpp {
+
+template<typename T>
+using storage = typename std::aligned_storage<sizeof(T), std::alignment_of<T>::value>::type;
+
+template<typename T>
+T& get(storage<T>& s) {
+  return *reinterpret_cast<T*>(&s);
+}
+
+template<typename T>
+const T& get(const storage<T>& s) {
+  return *reinterpret_cast<const T*>(&s);
+}
+
+} // namespace cpp
+
+#endif // CPP_STORAGE_H
