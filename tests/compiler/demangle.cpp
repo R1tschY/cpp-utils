946efe21cbae4f34c7f89d0daf81f2d07ab12680
R1tschY <r1tschy@yahoo.de>
2016-07-05 17:11:41 +0200

    fixed demangle test

--- tests/compiler/demangle.cpp
+++ tests/compiler/demangle.cpp
@@ -2,11 +2,13 @@
 #include <boost/test/unit_test.hpp>
 #include <boost/test/test_case_template.hpp>
 #include <boost/mpl/list.hpp>
+#include <iostream>
 
 #include <cpp-utils/compiler/demangle.h>
 
 namespace TestMe {
 
+template<typename T>
 struct A { virtual ~A() = default; };
 
 } // namespace TestMe
@@ -15,12 +17,14 @@ BOOST_AUTO_TEST_SUITE(Demangle_Tests)
 
 BOOST_AUTO_TEST_CASE(type_info)
 {
-  BOOST_CHECK_EQUAL(cpp::demangle(typeid(TestMe::A).name()), "TestMe::A");
+  BOOST_CHECK_EQUAL(cpp::demangle(typeid(TestMe::A<int>).name()), "TestMe::A<int>");
+  BOOST_CHECK_EQUAL(cpp::demangle_or_throw(typeid(TestMe::A<int>).name()), "TestMe::A<int>");
 }
 
-BOOST_AUTO_TEST_CASE(exception)
+BOOST_AUTO_TEST_CASE(invalid_argument)
 {
-  BOOST_CHECK_EQUAL(cpp::demangle(std::bad_exception().what()), "std::bad_exception");
+  BOOST_CHECK_EQUAL(cpp::demangle("std::size_t"), "");
+  BOOST_CHECK_THROW(cpp::demangle_or_throw("std::size_t"), std::invalid_argument);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
