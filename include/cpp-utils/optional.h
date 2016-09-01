d0b1470261dfda3012e7cecc0b7705364e5fda94
R1tschY <r1tschy@yahoo.de>
2016-09-01 19:55:20 +0200

    added cpp::optional

---
+++ include/cpp-utils/optional.h
@@ -0,0 +1,12 @@
+#ifndef LIBS_C___INCLUDE_CPP_UTILS_OPTIONAL_H_
+#define LIBS_C___INCLUDE_CPP_UTILS_OPTIONAL_H_
+
+#include <experimental/optional>
+
+namespace cpp {
+
+using std::experimental::optional;
+
+}  // namespace cpp
+
+#endif /* LIBS_C___INCLUDE_CPP_UTILS_OPTIONAL_H_ */
