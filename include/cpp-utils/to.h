d5671b7930ad6897fc255c86526e5aafd8b531e8
R1tschY <r1tschy@yahoo.de>
2017-06-05 17:43:20 +0200

    start of conversion framework

---
+++ include/cpp-utils/to.h
@@ -0,0 +1,51 @@
+#ifndef INCLUDE_CPP_UTILS_TO_H_
+#define INCLUDE_CPP_UTILS_TO_H_
+
+#include <type_traits>
+
+namespace cpp {
+
+// down_cast
+
+template<typename Src, typename BasePtr>
+typename std::enable_if<
+  std::is_base_of<std::remove_pointer_t<BasePtr>, Src>::value && std::is_pointer<BasePtr>,
+  std::remove_pointer_t<BasePtr>>::type
+to(Src* src)
+{
+  return src ? static_cast<BasePtr>(src) : nullptr;
+}
+
+template<typename Src, typename BaseRef>
+typename std::enable_if<
+  std::is_base_of<std::remove_reference_t<BaseRef>, Src>::value && std::is_reference<BaseRef>,
+  std::remove_reference_t<BaseRef>>::type
+to(Src& src)
+{
+  return static_cast<BaseRef>(src);
+}
+
+// upcast
+
+template<typename Base, typename DerivedPtr>
+typename std::enable_if<
+  std::is_base_of<Base, std::remove_pointer_t<DerivedPtr>>::value && std::is_pointer<DerivedPtr>,
+  std::remove_pointer_t<DerivedPtr>>::type
+to(Base* base)
+{
+  // use references for std::bad_cast
+  return base ? &dynamic_cast<DerivedPtr>(*base) : nullptr;
+}
+
+template<typename Base, typename DerivedPtr>
+typename std::enable_if<
+  std::is_base_of<Base, std::remove_reference_t<DerivedPtr>>::value && std::is_reference<DerivedPtr>,
+  std::remove_reference_t<DerivedPtr>>::type
+to(Base& base)
+{
+  return dynamic_cast<DerivedPtr>(base);
+}
+
+}  // namespace cpp
+
+#endif // INCLUDE_CPP_UTILS_TO_H_
